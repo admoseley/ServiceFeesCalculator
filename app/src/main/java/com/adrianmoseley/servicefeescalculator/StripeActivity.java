3584ce77cc97be7a7f389d7df409cddc63f9a186
Adrian Moseley <admoseley@hotmail.com>
2017-01-14 15:35:49 -0600

    All page calcuations work for each site.

--- app/src/main/java/com/adrianmoseley/servicefeescalculator/StripeActivity.java
+++ app/src/main/java/com/adrianmoseley/servicefeescalculator/StripeActivity.java
@@ -1,25 +1,91 @@
 package com.adrianmoseley.servicefeescalculator;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
-
+import android.widget.Toast;
 
 public class StripeActivity extends ActionBarActivity {
+    public double stripeAmount;
+    public double stripeFinal;
+    public double stripeFees;
+    public EditText stripeAmountET;
+    public TextView stripeFinalLargeTxt;
+    public TextView stripeFeesLargeTxt;
+    public Button stripecalculate;
+    public Button stripeclearbtn;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stripe);
+        stripeAmountET = (EditText) findViewById(R.id.stripeAmountEditText);
+        stripeFinalLargeTxt = (TextView) findViewById(R.id.stripeFinalText);
+        stripeFeesLargeTxt = (TextView) findViewById(R.id.stripeFeesLargeTxt);
+        stripecalculate = (Button) findViewById(R.id.stripecalculateBtn);
+        stripeclearbtn = (Button) findViewById(R.id.stripeclearbtn);
+        stripeAmountET.setFocusable(true);
         TextView tv = (TextView) findViewById(R.id.stripedisclaimer);
         tv.setText(Html.fromHtml("<b>Not Affiliated with STRIPE.COM</b>  " +
-                " <br /> <a href=\"https://stripe.com/us/pricing\"> Click here to view STRIPE Fees.</a>"));
+                " <br /> <a style=\"color:white\" href=\"https://stripe.com/us/pricing\"> Click here to view STRIPE Fees.</a>"));
         tv.setMovementMethod(LinkMovementMethod.getInstance());
 
+        stripecalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+
+                    stripeAmount = Double.parseDouble(stripeAmountET.getText().toString());
+
+
+                } catch (NumberFormatException e) {
+
+                    stripeAmount = 0.00;
+
+                }
+
+                if (stripeAmount == 0.00) {
+                    Toast.makeText(getApplicationContext(), "Please enter a value greater than Zero", Toast.LENGTH_LONG).show();
+                    stripeAmountET.setText("");
+                    stripeAmountET.setFocusable(true);
+                    stripeFinalLargeTxt.setText("0.00");
+                    stripeFeesLargeTxt.setText("0.00");
+
+                } else {
+                    stripeFinal = (stripeAmount + .3) / .971;
+                    stripeFees = (stripeFinal * .029) + .3;
+                    stripeFinalLargeTxt.setText(String.format("%.2f", stripeFinal));
+                    stripeFeesLargeTxt.setText(String.format("%.2f", stripeFees));
+                    stripeAmountET.setFocusable(true);
+
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(stripeAmountET.getWindowToken(), 0);
+                }
+            }
+        });
+
+        stripeclearbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stripeAmountET.setText("");
+                stripeFinalLargeTxt.setText("0.00");
+                stripeFeesLargeTxt.setText("0.00");
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(stripeAmountET.getWindowToken(), 0);
+
+            }
+        });
     }
 
     @Override
